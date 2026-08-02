7876610a70e1b1522df13d8ed9a1571b09d2a947
Vladislav <vladkoval039@gmail.com>
2026-08-02 11:35:04 +0300

    fix conf

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp -i "${DEPLOY_SSH_KEY}" ./.env "${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend/.env"`,
+      'pre-deploy-local': `scp -i ${DEPLOY_SSH_KEY} ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend/.env`,
       'post-deploy': 'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
